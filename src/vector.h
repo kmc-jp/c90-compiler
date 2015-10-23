4811991664ecb749c8a3d8f3b1b0246a7e210451
hatsusato <knightspm5@gmail.com>
2015-10-24 00:58:37 +0900

    Add struct Vector

--- src/vector.h
+++ src/vector.h
@@ -1,11 +1,22 @@
 #ifndef KMC_C89_COMPILER_VECTOR_H
 #define KMC_C89_COMPILER_VECTOR_H
 
+/* to make compilable */
+#define Type int
+
 #define CONCATENATE(x, y) x ## y
 #define CONCAT(x, y) CONCATENATE(x, y)
 #define WITHBAR(x) CONCAT(x, _)
 
 /* template macro */
 #define T(identifier, type) CONCAT(WITHBAR(identifier), WITHBAR(type))
+#define V(type) T(Vector, type)
+
+/* vector of Type */
+struct V(Type) {
+  Type* start_;  /* first data */
+  Type* finish_;  /* last data */
+  Type* end_;  /* end of storage */
+};
 
 #endif  /* KMC_C89_COMPILER_VECTOR_H */
