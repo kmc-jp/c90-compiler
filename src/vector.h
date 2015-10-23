d86cb1d389deb02351362a20b76f858065ac17b3
hatsusato <knightspm5@gmail.com>
2015-10-24 01:28:10 +0900

    Add template macro

--- src/vector.h
+++ src/vector.h
@@ -1,4 +1,11 @@
 #ifndef KMC_C89_COMPILER_VECTOR_H
 #define KMC_C89_COMPILER_VECTOR_H
 
+#define CONCATENATE(x, y) x ## y
+#define CONCAT(x, y) CONCATENATE(x, y)
+#define WITHBAR(x) CONCAT(x, _)
+
+/* template macro */
+#define T(identifier, type) CONCAT(WITHBAR(identifier), WITHBAR(type))
+
 #endif  /* KMC_C89_COMPILER_VECTOR_H */
