9f556758d4dfd4f04ca4ff186410038b9277bc35
hatsusato <knightspm5@gmail.com>
2015-10-28 00:34:37 +0900

    Use VECTORREF instead of VR

--- src/vector.h
+++ src/vector.h
@@ -13,7 +13,7 @@ typedef signed char bool;
 /* template macro */
 #define TEMPLATE(type, identifier) CONCAT(WITHBAR(identifier), WITHBAR(type))
 #define VECTOR(type) TEMPLATE(type, Vector)
-#define VR(type) TEMPLATE(type, VectorRef)
+#define VECTORREF(type) TEMPLATE(type, VectorRef)
 #define VECTOR_GLOBAL(type) CONCAT(global_, TEMPLATE(type, VectorMethods))
 #define VECTOR_METHOD(type, function) VECTOR_GLOBAL(type).WITHBAR(function)
 #define VF(function, type) TEMPLATE(type, CONCAT(Vector, function))
@@ -43,48 +43,54 @@ typedef signed char bool;
     Type* end_;  /* end of storage */                                   \
   };                                                                    \
   /* reference to vector of Type */                                     \
-  typedef struct VECTOR(Type)* VR(Type);                                \
+  typedef struct VECTOR(Type)* VECTORREF(Type);                         \
                                                                         \
   /* constructor */                                                     \
-  VR(Type) VF(Ctor, Type)(void);                                        \
+  VECTORREF(Type) VF(Ctor, Type)(void);                                 \
   /* destructor */                                                      \
-  void VF(Dtor, Type)(VR(Type)* pthis);                                 \
+  void VF(Dtor, Type)(VECTORREF(Type)* pthis);                          \
   /* operator= */                                                       \
-  void VF(Copy, Type)(VR(Type) this, VR(Type) src);                     \
+  void VF(Copy, Type)(VECTORREF(Type) this, VECTORREF(Type) src);       \
   /* assign */                                                          \
-  void VF(Assign, Type)(VR(Type) this, Type* data, size_t count);       \
+  void VF(Assign, Type)(VECTORREF(Type) this,                           \
+                        Type* data, size_t count);                      \
   /* data */                                                            \
-  Type* VF(Data, Type)(VR(Type) this);                                  \
+  Type* VF(Data, Type)(VECTORREF(Type) this);                           \
   /* begin */                                                           \
-  Type* VF(Begin, Type)(VR(Type) this);                                 \
+  Type* VF(Begin, Type)(VECTORREF(Type) this);                          \
   /* end */                                                             \
-  Type* VF(End, Type)(VR(Type) this);                                   \
+  Type* VF(End, Type)(VECTORREF(Type) this);                            \
   /* empty */                                                           \
-  bool VF(Empty, Type)(VR(Type) this);                                  \
+  bool VF(Empty, Type)(VECTORREF(Type) this);                           \
   /* size */                                                            \
-  size_t VF(Size, Type)(VR(Type) this);                                 \
+  size_t VF(Size, Type)(VECTORREF(Type) this);                          \
   /* reserve */                                                         \
-  void VF(Reserve, Type)(VR(Type) this, size_t size);                   \
+  void VF(Reserve, Type)(VECTORREF(Type) this, size_t size);            \
   /* capacity */                                                        \
-  size_t VF(Capacity, Type)(VR(Type) this);                             \
+  size_t VF(Capacity, Type)(VECTORREF(Type) this);                      \
   /* clear */                                                           \
-  void VF(Clear, Type)(VR(Type) this);                                  \
+  void VF(Clear, Type)(VECTORREF(Type) this);                           \
   /* insert */                                                          \
-  /* insert 'count' elements in data to the position indexed by 'pos' */ \
-  void VF(Insert, Type)(VR(Type) this, size_t pos, Type* data, size_t count); \
+  /* insert 'count' elements in data                                    \
+     to the position indexed by 'pos' */                                \
+  void VF(Insert, Type)(VECTORREF(Type) this,                           \
+                        size_t pos, Type* data, size_t count);          \
   /* erase */                                                           \
-  /* remove consecutive 'count' elements whose head is indexed by 'pos' */ \
-  void VF(Erase, Type)(VR(Type) this, size_t pos, size_t count);        \
+  /* remove consecutive 'count' elements                                \
+     whose head is indexed by 'pos' */                                  \
+  void VF(Erase, Type)(VECTORREF(Type) this,                            \
+                       size_t pos, size_t count);                       \
   /* push_back */                                                       \
-  void VF(Push, Type)(VR(Type) this, Type* value);                      \
+  void VF(Push, Type)(VECTORREF(Type) this, Type* value);               \
   /* pop_back */                                                        \
   /* calling pop_back on empty vector is undefined */                   \
-  void VF(Pop, Type)(VR(Type) this);                                    \
+  void VF(Pop, Type)(VECTORREF(Type) this);                             \
   /* resize */                                                          \
   /* append copies of 'value' when extension */                         \
-  void VF(Resize, Type)(VR(Type) this, size_t size, Type* value);       \
+  void VF(Resize, Type)(VECTORREF(Type) this,                           \
+                        size_t size, Type* value);                      \
   /* swap */                                                            \
-  void VF(Swap, Type)(VR(Type) lhs, VR(Type) rhs);                      \
+  void VF(Swap, Type)(VECTORREF(Type) lhs, VECTORREF(Type) rhs);        \
 
 #define DEFINE_VECTOR(Type)                                             \
   struct TEMPLATE(Type, VectorMethods) VECTOR_GLOBAL(Type);             \
@@ -98,13 +104,13 @@ typedef signed char bool;
     VECTOR_METHOD(Type, copy) = copy;                                   \
   }                                                                     \
                                                                         \
-  void VF(Free, Type)(VR(Type) this) {                                  \
+  void VF(Free, Type)(VECTORREF(Type) this) {                           \
     free(this->start_);                                                 \
   }                                                                     \
-  void VF(Nullify, Type)(VR(Type) this) {                               \
+  void VF(Nullify, Type)(VECTORREF(Type) this) {                        \
     this->start_ = this->finish_ = this->end_ = NULL;                   \
   }                                                                     \
-  Type* VF(SetEnd, Type)(VR(Type) this, Type* end) {                    \
+  Type* VF(SetEnd, Type)(VECTORREF(Type) this, Type* end) {             \
     return this->finish_ = end;                                         \
   }                                                                     \
   void VF(MoveUp, Type)(Type* head, Type* tail, size_t count) {         \
@@ -153,7 +159,8 @@ typedef signed char bool;
     }                                                                   \
   }                                                                     \
   /* this->start_ must not be allocated */                              \
-  void VF(New, Type)(VR(Type) this, size_t size, Type* data, size_t count) { \
+  void VF(New, Type)(VECTORREF(Type) this,                              \
+                     size_t size, Type* data, size_t count) {           \
     const size_t capacity = EnoughCapacity(size);                       \
     this->start_ = (Type*)malloc(sizeof(Type) * capacity);              \
     this->finish_ = this->start_ + count;                               \
@@ -161,23 +168,25 @@ typedef signed char bool;
     VF(RangeCtorCopy, Type)(this->start_, this->finish_, data);         \
   }                                                                     \
                                                                         \
-  VR(Type) VF(Ctor, Type)(void) {                                       \
-    VR(Type) this = (VR(Type))malloc(sizeof(struct VECTOR(Type)));      \
+  VECTORREF(Type) VF(Ctor, Type)(void) {                                \
+    VECTORREF(Type) this =                                              \
+        (VECTORREF(Type))malloc(sizeof(struct VECTOR(Type)));           \
     VF(Nullify, Type)(this);                                            \
     return this;                                                        \
   }                                                                     \
-  void VF(Dtor, Type)(VR(Type)* pthis) {                                \
+  void VF(Dtor, Type)(VECTORREF(Type)* pthis) {                         \
     assert(pthis && *pthis);                                            \
     VF(Clear, Type)(*pthis);                                            \
     VF(Free, Type)(*pthis);                                             \
     free(*pthis);                                                       \
     *pthis = NULL;                                                      \
   }                                                                     \
-  void VF(Copy, Type)(VR(Type) this, VR(Type) src) {                    \
+  void VF(Copy, Type)(VECTORREF(Type) this, VECTORREF(Type) src) {      \
     assert(this && src);                                                \
     VF(Assign, Type)(this, VF(Data, Type)(src), VF(Size, Type)(src));   \
   }                                                                     \
-  void VF(Assign, Type)(VR(Type) this, Type* data, size_t count) {      \
+  void VF(Assign, Type)(VECTORREF(Type) this,                           \
+                        Type* data, size_t count) {                     \
     assert(this);                                                       \
     assert(count == 0 || data);                                         \
     if (VF(Capacity, Type)(this) < count) {                             \
@@ -197,27 +206,27 @@ typedef signed char bool;
       }                                                                 \
     }                                                                   \
   }                                                                     \
-  Type* VF(Data, Type)(VR(Type) this) {                                 \
+  Type* VF(Data, Type)(VECTORREF(Type) this) {                          \
     assert(this);                                                       \
     return this->start_;                                                \
   }                                                                     \
-  Type* VF(Begin, Type)(VR(Type) this) {                                \
+  Type* VF(Begin, Type)(VECTORREF(Type) this) {                         \
     assert(this);                                                       \
     return this->start_;                                                \
   }                                                                     \
-  Type* VF(End, Type)(VR(Type) this) {                                  \
+  Type* VF(End, Type)(VECTORREF(Type) this) {                           \
     assert(this);                                                       \
     return this->finish_;                                               \
   }                                                                     \
-  bool VF(Empty, Type)(VR(Type) this) {                                 \
+  bool VF(Empty, Type)(VECTORREF(Type) this) {                          \
     assert(this);                                                       \
     return this->start_ == this->finish_;                               \
   }                                                                     \
-  size_t VF(Size, Type)(VR(Type) this) {                                \
+  size_t VF(Size, Type)(VECTORREF(Type) this) {                         \
     assert(this);                                                       \
     return this->finish_ - this->start_;                                \
   }                                                                     \
-  void VF(Reserve, Type)(VR(Type) this, size_t size) {                  \
+  void VF(Reserve, Type)(VECTORREF(Type) this, size_t size) {           \
     assert(this);                                                       \
     if (VF(Capacity, Type)(this) < size) {                              \
       const size_t old_size = VF(Size, Type)(this);                     \
@@ -225,18 +234,18 @@ typedef signed char bool;
         VF(Free, Type)(this);                                           \
         VF(New, Type)(this, size, NULL, 0);                             \
       } else {                                                          \
-        VR(Type) buffer = VF(Ctor, Type)();                             \
+        VECTORREF(Type) buffer = VF(Ctor, Type)();                      \
         VF(New, Type)(buffer, size, VF(Data, Type)(this), old_size);    \
         VF(Swap, Type)(this, buffer);                                   \
         VF(Dtor, Type)(&buffer);                                        \
       }                                                                 \
     }                                                                   \
   }                                                                     \
-  size_t VF(Capacity, Type)(VR(Type) this) {                            \
+  size_t VF(Capacity, Type)(VECTORREF(Type) this) {                     \
     assert(this);                                                       \
     return this->end_ - this->start_;                                   \
   }                                                                     \
-  void VF(Clear, Type)(VR(Type) this) {                                 \
+  void VF(Clear, Type)(VECTORREF(Type) this) {                          \
     assert(this);                                                       \
     {                                                                   \
       Type* const begin = VF(Begin, Type)(this);                        \
@@ -245,7 +254,8 @@ typedef signed char bool;
       VF(RangeDtor, Type)(begin, end);                                  \
     }                                                                   \
   }                                                                     \
-  void VF(Insert, Type)(VR(Type) this, size_t pos, Type* data, size_t count) { \
+  void VF(Insert, Type)(VECTORREF(Type) this,                           \
+                        size_t pos, Type* data, size_t count) {         \
     assert(this);                                                       \
     {                                                                   \
       const size_t size = VF(Size, Type)(this);                         \
@@ -264,7 +274,8 @@ typedef signed char bool;
       }                                                                 \
     }                                                                   \
   }                                                                     \
-  void VF(Erase, Type)(VR(Type) this, size_t pos, size_t count) {       \
+  void VF(Erase, Type)(VECTORREF(Type) this,                            \
+                       size_t pos, size_t count) {                      \
     assert(this);                                                       \
     {                                                                   \
       const size_t size = VF(Size, Type)(this);                         \
@@ -280,7 +291,7 @@ typedef signed char bool;
       }                                                                 \
     }                                                                   \
   }                                                                     \
-  void VF(Push, Type)(VR(Type) this, Type* value) {                     \
+  void VF(Push, Type)(VECTORREF(Type) this, Type* value) {              \
     assert(this && value);                                              \
     VF(Reserve, Type)(this, VF(Size, Type)(this) + 1);                  \
     {                                                                   \
@@ -289,7 +300,7 @@ typedef signed char bool;
       VF(RangeCtorCopy, Type)(end, new_end, value);                     \
     }                                                                   \
   }                                                                     \
-  void VF(Pop, Type)(VR(Type) this) {                                   \
+  void VF(Pop, Type)(VECTORREF(Type) this) {                            \
     assert(this);                                                       \
     {                                                                   \
       Type* const end = VF(End, Type)(this);                            \
@@ -297,7 +308,8 @@ typedef signed char bool;
       VF(RangeDtor, Type)(new_end, end);                                \
     }                                                                   \
   }                                                                     \
-  void VF(Resize, Type)(VR(Type) this, size_t size, Type* value) {      \
+  void VF(Resize, Type)(VECTORREF(Type) this,                           \
+                        size_t size, Type* value) {                     \
     assert(this);                                                       \
     {                                                                   \
       const size_t old_size = VF(Size, Type)(this);                     \
@@ -317,7 +329,7 @@ typedef signed char bool;
       }                                                                 \
     }                                                                   \
   }                                                                     \
-  void VF(Swap, Type)(VR(Type) lhs, VR(Type) rhs) {                     \
+  void VF(Swap, Type)(VECTORREF(Type) lhs, VECTORREF(Type) rhs) {       \
     assert(lhs && rhs);                                                 \
     {                                                                   \
       struct VECTOR(Type) tmp = *lhs;                                   \
