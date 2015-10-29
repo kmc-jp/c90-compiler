9582778b0135467e401c495053b996e08d47e786
hatsusato <knightspm5@gmail.com>
2015-10-30 01:49:44 +0900

    Add utility sources
    Add boolean constants

---
+++ src/utility.h
@@ -0,0 +1,12 @@
+#ifndef KMC_C89_COMPILER_UTILITY_H
+#define KMC_C89_COMPILER_UTILITY_H
+
+#include <stddef.h>
+
+#ifndef __cplusplus
+typedef signed char bool;
+extern const bool true;
+extern const bool false;
+#endif  /* __cplusplus */
+
+#endif  /* KMC_C89_COMPILER_UTILITY_H */
