95fdbef1e7c3b5f8bc20bf2896673f851c0b544e
hatsusato <knightspm5@gmail.com>
2015-10-29 22:15:17 +0900

    Add LLVMBuilder

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char *argv[]) {
   LLVMModuleRef module = LLVMModuleCreateWithName("kmc89_module");
+  LLVMBuilderRef builder = LLVMCreateBuilder();
+  LLVMDisposeBuilder(builder);
   LLVMDisposeModule(module);
   return 0;
 }
