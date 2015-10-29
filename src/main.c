2c178e0107d02f01553c979bd220d58e5f79241b
hatsusato <knightspm5@gmail.com>
2015-10-30 01:51:20 +0900

    Create main function in llvm
    main takes no arugment and return only constant 0

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <llvm-c/Core.h>
+#include "utility.h"
 
 int main(int argc, char *argv[]) {
   LLVMModuleRef module = LLVMModuleCreateWithName("kmc89_module");
+  /* int main() { return 0; } */
+  LLVMTypeRef main_type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, false);
+  LLVMValueRef main_func = LLVMAddFunction(module, "main", main_type);
+  LLVMBasicBlockRef main_entry = LLVMAppendBasicBlock(main_func, "main_entry");
   LLVMBuilderRef builder = LLVMCreateBuilder();
+  LLVMPositionBuilderAtEnd(builder, main_entry);
+  LLVMValueRef return_value = LLVMConstInt(LLVMInt32Type(), 0, false);
+  LLVMBuildRet(builder, return_value);
   LLVMDisposeBuilder(builder);
   LLVMDisposeModule(module);
   return 0;
