9918012050d35ffd77f3bd5b95bbdcac9095b40b
hatsusato <knightspm5@gmail.com>
2015-10-31 02:22:37 +0900

    Add copy test

--- tests/vector_test/vector_int_test.cpp
+++ tests/vector_test/vector_int_test.cpp
@@ -29,6 +29,22 @@ TEST_F(VectorIntTest, dtor) {
   EXPECT_EQ(NULL, vec);
 }
 
+TEST_F(VectorIntTest, copy) {
+  const int data[] = {0, 1, 2, 3, 4};
+  const size_t count = sizeof(data) / sizeof(int);
+  size_t i = 0;
+  VECTORREF(int) v2 = VECINT(ctor)();
+  VECINT(assign)(v, data, count);
+  VECINT(copy)(v2, v);
+  ASSERT_TRUE(VECINT(data)(v2) != NULL);
+  for (i = 0; i < count; ++i) {
+    EXPECT_EQ((int)i, VECINT(data)(v2)[i]);
+  }
+  EXPECT_FALSE(VECINT(empty)(v2));
+  EXPECT_EQ(count, VECINT(size)(v2));
+  EXPECT_LE(count, VECINT(capacity)(v2));
+}
+
 TEST_F(VectorIntTest, assign) {
   const int data[] = {0, 1, 2, 3, 4};
   const size_t count = sizeof(data) / sizeof(int);
