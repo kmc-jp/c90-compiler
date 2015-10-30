f5dabf16443daeb061aff120dd2615c81ead00fa
hatsusato <knightspm5@gmail.com>
2015-10-31 02:11:43 +0900

    Add VectorIntEnvironment
    Register the global environment to google test

--- tests/vector_test/main.cpp
+++ tests/vector_test/main.cpp
@@ -1,6 +1,16 @@
 #include "gtest/gtest.h"
+extern "C" {
+#include "vector_int.h"
+}
+
+class VectorIntEnvironment : public ::testing::Environment {
+  virtual void SetUp() {
+    INITIALIZE_DEFAULT_METHODS(int);
+  }
+};
 
 GTEST_API_ int main(int argc, char **argv) {
+  ::testing::AddGlobalTestEnvironment(new VectorIntEnvironment);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
