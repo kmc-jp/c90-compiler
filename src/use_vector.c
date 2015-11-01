1f5069e661407658aab99355b8541a684b45f929
hatsusato <knightspm5@gmail.com>
2015-11-01 16:34:24 +0900

    Add initialize_vector

--- src/use_vector.c
+++ src/use_vector.c
@@ -2,3 +2,7 @@
 
 DEFINE_VECTOR(char)
 DEFINE_DEFAULT_METHODS(char)
+
+void initialize_vector() {
+  INITIALIZE_DEFAULT_METHODS(char);
+}
--- src/use_vector.h
+++ src/use_vector.h
@@ -6,4 +6,9 @@
 DECLARE_VECTOR(char)
 DECLARE_DEFAULT_METHODS(char)
 
+/* Add initialization for VECTORREF(Type) to this */
+/* Then call this at the head of execution
+   like at the beginning of main */
+void initialize_vector();
+
 #endif  /* KMC_C89_COMPILER_USE_VECTOR_H */
