031130c6f78e7a792db9f2c82f29b1dc013d242a
hatsusato <knightspm5@gmail.com>
2015-11-01 18:38:30 +0900

    Rename safe_malloc to safe_malloc_impl

--- src/utility.h
+++ src/utility.h
@@ -11,7 +11,8 @@ extern const bool true;
 extern const bool false;
 #endif  /* __cplusplus */
 
-void* safe_malloc(size_t size);
+void* safe_malloc_impl(size_t size);
+
 void safe_free_impl(void** ptr);
 #define safe_free(ptr)                          \
   do { safe_free_impl(&ptr); } while(false)
