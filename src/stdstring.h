1105cc4f4c6e45ea630443c64d29ba1c49ef3b92
hatsusato <knightspm5@gmail.com>
2015-11-05 21:33:14 +0900

    Add access member function prototypes

--- src/stdstring.h
+++ src/stdstring.h
@@ -1,6 +1,8 @@
 #ifndef KMC_C89_COMPILER_STDSTRING_H
 #define KMC_C89_COMPILER_STDSTRING_H
 
+#include "utility.h"
+
 typedef struct String* StringRef;
 
 /* constructor */
@@ -14,5 +16,22 @@ void string_dtor(StringRef* pself);
 void string_copy(StringRef self, StringRef src);
 /* assign */
 void string_assign(StringRef self, const char* src);
+/* at */
+/* behavior is undefined if index is out of range */
+char string_at(StringRef self, size_t index);
+/* front */
+/* return null-character if self is empty */
+char string_front(StringRef self);
+/* back */
+/* return null-character if self is empty */
+char string_back(StringRef self);
+/* data */
+char* string_data(StringRef self);
+/* begin */
+/* same as data */
+char* string_begin(StringRef self);
+/* end */
+/* return pointer to the null character following the last character */
+char* string_end(StringRef self);
 
 #endif  /* KMC_C89_COMPILER_STDSTRING_H */
