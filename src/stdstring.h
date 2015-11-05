d97040f0147b83c2a6ffee864291db869cb5a474
hatsusato <knightspm5@gmail.com>
2015-11-05 21:32:08 +0900

    Add fundamental member function prototypes

--- src/stdstring.h
+++ src/stdstring.h
@@ -3,4 +3,16 @@
 
 typedef struct String* StringRef;
 
+/* constructor */
+/* returned object is intialized by src */
+/* returned object must be destructed by string_dtor */
+StringRef string_ctor(const char* src);
+/* destructor */
+/* *pself is set to NULL after destruction */
+void string_dtor(StringRef* pself);
+/* operator= */
+void string_copy(StringRef self, StringRef src);
+/* assign */
+void string_assign(StringRef self, const char* src);
+
 #endif  /* KMC_C89_COMPILER_STDSTRING_H */
