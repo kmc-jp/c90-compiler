2a0cf754c2184db23802bcd79ea740a8ac835701
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-11-06 10:36:32 +0900

    add lexer_adapter

---
+++ src/lexer_adapter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lex.yy.h"
+#include "yacc_mock.h"
+#include "lexer_adapter.h"
+
+void yyin_from_file(const char *filename) {
+  FILE* fp = fopen(filename, "r");
+  if (fp == NULL) {
+    printf("fatal error: failed to open %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+  yyin = fp;
+}
+
+void yyin_from_string(char *code, int size) {
+  FILE *fp = fmemopen(code, size + 1, "r");
+  if (fp == NULL) {
+    printf("fatal error: failed to open stream\n");
+    exit(EXIT_FAILURE);
+  }
+}
