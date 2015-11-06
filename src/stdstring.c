8a00c5e9e0e032f5254054e1c4f3a07a8d4c8d2c
hatsusato <knightspm5@gmail.com>
2015-11-07 02:54:11 +0900

    Add string_ctor

--- src/stdstring.c
+++ src/stdstring.c
@@ -1,4 +1,5 @@
 #include "stdstring.h"
+#include <string.h>
 
 struct String {
   char* data_;
@@ -6,3 +7,12 @@ struct String {
 };
 
 const size_t string_npos = (size_t)(-1);
+
+StringRef string_ctor(const char* src) {
+  const size_t length = src ? strlen(src) : 0;
+  const StringRef self = safe_malloc(struct String);
+  self->data_ = safe_array_malloc(char, length + 1);
+  strncpy(self->data_, src, length + 1);
+  self->length_ = length;
+  return self;
+}
