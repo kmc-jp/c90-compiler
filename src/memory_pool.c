25d746255876e136109a722b7ccdb3e43aa337f7
hatsusato <knightspm5@gmail.com>
2015-11-13 18:09:34 +0900

    Implement memory_pool_ctor

--- src/memory_pool.c
+++ src/memory_pool.c
@@ -19,3 +19,17 @@ struct MemoryPool {
   MemoryPoolLargeRef large_;
   size_t max_;
 };
+
+MemoryPoolRef memory_pool_ctor(size_t size) {
+  const size_t header_size = sizeof(struct MemoryPoolBlock);
+  const size_t block_size = MAX(size, header_size * 2);
+  const MemoryPoolRef pool = safe_malloc(struct MemoryPool);
+  byte* const block = safe_array_malloc(byte, block_size);
+  pool->block_ = (MemoryPoolBlockRef)block;
+  pool->block_->begin_ = block + header_size;
+  pool->block_->end_ = block + block_size;
+  pool->block_->prev_ = NULL;
+  pool->large_ = NULL;
+  pool->max_ = block_size - header_size;
+  return pool;
+}
