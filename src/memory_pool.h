572c63ab6d16063da0780700cad5a8d871ba3bcd
hatsusato <knightspm5@gmail.com>
2015-11-13 17:28:03 +0900

    Add memory pool ctor and dtor prototypes

--- src/memory_pool.h
+++ src/memory_pool.h
@@ -5,4 +5,10 @@
 
 typedef struct MemoryPool* MemoryPoolRef;
 
+/* create memory pool
+   each block size is 'size' */
+MemoryPoolRef memory_pool_ctor(size_t size);
+/* destroy memory pool */
+void memory_pool_dtor(MemoryPoolRef* ppool);
+
 #endif  /* KMC_C89_COMPILER_MEMORY_POOL_H */
