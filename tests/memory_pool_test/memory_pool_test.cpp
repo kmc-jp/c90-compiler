2f4dd05fdbdd1785f55b84c5d2ee728db7407747
hatsusato <knightspm5@gmail.com>
2015-11-13 22:43:21 +0900

    Add strings test

--- tests/memory_pool_test/memory_pool_test.cpp
+++ tests/memory_pool_test/memory_pool_test.cpp
@@ -36,3 +36,33 @@ TEST_F(MemoryPoolTest, ctor_and_dtor) {
   memory_pool_dtor(&pool);
   EXPECT_EQ(NULL, pool);
 }
+
+TEST_F(MemoryPoolTest, strings) {
+  const char* const expected =
+      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int i = 0;
+  int j = 0;
+  char** const ptrs = palloc(char*, p, 100);
+  for (i = 0; i < 100; ++i) {
+    char* const str = ptrs[i] = palloc(char, p, 64);
+    EXPECT_TRUE(IS_ALIGNED(char, str));
+    for (j = 0; j < 10; ++j) {
+      str[j] = '0' + j;
+    }
+    for (j = 0; j < 26; ++j) {
+      str[j + 10] = 'a' + j;
+    }
+    for (j = 0; j < 26; ++j) {
+      str[j + 36] = 'A' + j;
+    }
+    for (j = 62; j < 64; ++j) {
+      str[j] = '\0';
+    }
+    EXPECT_STREQ(expected, str);
+  }
+  for (i = 0; i < 100; ++i) {
+    for (j = i + 1; j < 100; ++j) {
+      EXPECT_NE(ptrs[i], ptrs[j]);
+    }
+  }
+}
