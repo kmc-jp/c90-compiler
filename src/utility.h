108e452a282cafec8b7902d753e65abaffa604b5
hatsusato <knightspm5@gmail.com>
2015-11-13 17:23:54 +0900

    Add MIN and MAX macro

--- src/utility.h
+++ src/utility.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 
 #define UNUSED(x) (void)(x)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) < (y) ? (y) : (x))
 
 #ifndef __cplusplus
 typedef signed char bool;
