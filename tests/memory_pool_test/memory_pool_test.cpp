126a78824c3042ace80a4c23581d88992fc80726
hatsusato <knightspm5@gmail.com>
2015-11-13 21:51:23 +0900

    Add MemoryPoolTest fixture

--- tests/memory_pool_test/memory_pool_test.cpp
+++ tests/memory_pool_test/memory_pool_test.cpp
@@ -1,1 +1,13 @@
 #include "gtest/gtest.h"
+extern "C" {
+#include "memory_pool.h"
+}
+
+class MemoryPoolTest : public ::testing::Test {
+ protected:
+  MemoryPoolTest() : p(memory_pool_ctor(1024)) {}
+  ~MemoryPoolTest() {
+    memory_pool_dtor(&p);
+  }
+  MemoryPoolRef p;
+};
