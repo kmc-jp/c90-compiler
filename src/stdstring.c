ec42adf55f91bc3f845a6d4f135b1e117a51151c
hatsusato <knightspm5@gmail.com>
2015-11-14 22:19:40 +0900

    Re-order utility static functions

--- src/stdstring.c
+++ src/stdstring.c
@@ -10,6 +10,13 @@ struct String {
 
 const size_t string_npos = (size_t)(-1);
 
+static void string_free(StringRef self) {
+  safe_free(self->data_);
+}
+static void string_init(StringRef self, const char* src, size_t length) {
+  strncpy(self->data_, src, length + 1);
+  self->length_ = length;
+}
 static void string_alloc(StringRef self, size_t size) {
   self->data_ = safe_array_malloc(char, size);
   self->capacity_ = size - 1;
@@ -18,17 +25,10 @@ static void string_extend(StringRef self, size_t size) {
   string_free(self);
   string_alloc(self, enough_capacity(size + 1));
 }
-static void string_init(StringRef self, const char* src, size_t length) {
-  strncpy(self->data_, src, length + 1);
-  self->length_ = length;
-}
 static void string_new(StringRef self, const char* src,
                        size_t length, size_t size) {
   string_alloc(self, enough_capacity(size + 1));
   string_init(self, src, length);
-}
-static void string_free(StringRef self) {
-  safe_free(self->data_);
 }
 
 StringRef string_ctor(const char* src) {
