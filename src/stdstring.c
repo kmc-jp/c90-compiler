298e9a925e7886c1f39ea921152438737e2115f1
hatsusato <knightspm5@gmail.com>
2015-11-14 22:57:09 +0900

    Add string_shrink_to_fit

--- src/stdstring.c
+++ src/stdstring.c
@@ -125,3 +125,16 @@ size_t string_capacity(StringRef self) {
   assert(self);
   return self->capacity_;
 }
+
+void string_shrink_to_fit(StringRef self) {
+  assert(self);
+  {
+    const size_t length = string_length(self);
+    if (length < string_capacity(self)) {
+      char* old_data = string_data(self);
+      string_alloc(self, length + 1);
+      string_init(self, old_data, length);
+      safe_free(old_data);
+    }
+  }
+}
