64cdca36406c06286d93aed163b1bd6acc90418d
hatsusato <knightspm5@gmail.com>
2015-11-20 02:13:34 +0900

    Add AstTag and Ast

--- src/ast.h
+++ src/ast.h
@@ -1,4 +1,15 @@
 #ifndef KMC_C89_COMPILER_AST_H
 #define KMC_C89_COMPILER_AST_H
 
+enum AstTag {
+};
+
+struct Ast {
+  enum AstTag tag;
+  union AstBase {
+  } ast;
+};
+
+typedef struct Ast AST;
+
 #endif  /* KMC_C89_COMPILER_AST_H */
