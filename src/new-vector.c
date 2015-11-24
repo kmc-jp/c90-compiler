8e76c356d3bd51ff6cb1171e59c7cebd8c86c288
hatsusato <knightspm5@gmail.com>
2015-11-24 21:30:14 +0900

    Add data structure

--- src/new-vector.c
+++ src/new-vector.c
@@ -1,1 +1,8 @@
 #include "new-vector.h"
+
+/* vector of Type */
+struct Vector {
+  Type* start_;  /* first data */
+  Type* finish_;  /* last data */
+  Type* end_;  /* end of storage */
+};
--- src/new-vector.h
+++ src/new-vector.h
@@ -1,4 +1,9 @@
 #ifndef KMC_C89_COMPILER_NEW_VECTOR_H
 #define KMC_C89_COMPILER_NEW_VECTOR_H
 
+typedef int Type;
+
+/* reference to vector of Type */
+typedef struct Vector* VectorRef;
+
 #endif  /* KMC_C89_COMPILER_NEW_VECTOR_H */
