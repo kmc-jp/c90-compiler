bdfc7c5b7647880db8434aa582a2821158c39a06
hatsusato <knightspm5@gmail.com>
2015-11-25 00:46:18 +0900

    Add vector_erase

--- src/new-vector.c
+++ src/new-vector.c
@@ -143,3 +143,15 @@ void vector_insert(VectorRef self, size_t index,
     vector_set_size(size + count);
   }
 }
+void vector_erase(VectorRef self, size_t index, size_t count) {
+  assert(self);
+  if (index + count < vector_size(self)) {
+    const size_t new_size = vector_size(self) - count;
+    Type* const head = vector_begin(self) + index;
+    Type* const tail = head + count;
+    VECTOR_MEMORY_MOVE(head, tail, new_size - index);
+    vector_set_size(new_size);
+  } else {
+    vector_set_size(index);
+  }
+}
