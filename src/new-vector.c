c22c39b381eee2d291532a99036ae6fddd8ef428
hatsusato <knightspm5@gmail.com>
2015-11-24 21:48:45 +0900

    Add vectorref_alloc

--- src/new-vector.c
+++ src/new-vector.c
@@ -6,3 +6,9 @@ struct Vector {
   Type* finish_;  /* last data */
   Type* end_;  /* end of storage */
 };
+
+static VectorRef vectorref_alloc(void) {
+  const VectorRef self = safe_malloc(struct Vector);
+  self->start_ = self->finish_ = self->end_ = NULL;
+  return self;
+}
