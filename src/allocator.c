8fa4b019efb41f60f8970c422ecccb68afbd612f
hatsusato <knightspm5@gmail.com>
2015-11-26 17:17:02 +0900

    Add allcator member functions

--- src/allocator.c
+++ src/allocator.c
@@ -6,3 +6,13 @@ struct Allocator {
   void* (*element_allocator_)(size_t count, void* manager);
   void (*deallocator_)(void* ptr, void* manager);
 };
+
+void* allocate_container(AllocatorRef allocator) {
+  return allocator->container_allocator_(allocator->manager_);
+}
+void* allocate_element(AllocatorRef allocator, size_t count) {
+  return allocator->element_allocator_(count, allocator->manager_);
+}
+void deallocate(AllocatorRef allocator, void* ptr) {
+  allocator->deallocator_(ptr, allocator->manager_);
+}
--- src/allocator.h
+++ src/allocator.h
@@ -4,4 +4,8 @@
 /* reference to allocator */
 typedef const struct Allocator* AllocatorRef;
 
+void* allocate_container(AllocatorRef allocator);
+void* allocate_element(AllocatorRef allocator, size_t count);
+void deallocate(AllocatorRef allocator, void* ptr);
+
 #endif  /* KMC_C89_COMPILER_ALLOCATOR_H */
