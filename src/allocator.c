9bcedca3387cbca21e9da9eddbadbe7082c1c7ce
hatsusato <knightspm5@gmail.com>
2015-11-26 17:55:25 +0900

    Move Allocator definition to allocator_impl.h

--- src/allocator.c
+++ src/allocator.c
@@ -1,11 +1,5 @@
 #include "allocator.h"
-
-struct Allocator {
-  void* manager_;
-  void* (*container_allocator_)(void* manager);
-  void* (*element_allocator_)(size_t count, void* manager);
-  void (*deallocator_)(void* ptr, void* manager);
-};
+#include "allocator_impl.h"
 
 void* allocate_container(AllocatorRef allocator) {
   return allocator->container_allocator_(allocator->manager_);
