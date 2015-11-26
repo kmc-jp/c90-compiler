8f0aef5b4420e95dd521b20262a8457edce83825
hatsusato <knightspm5@gmail.com>
2015-11-26 17:14:44 +0900

    Add allocator data structure

--- src/allocator.c
+++ src/allocator.c
@@ -1,1 +1,8 @@
 #include "allocator.h"
+
+struct Allocator {
+  void* manager_;
+  void* (*container_allocator_)(void* manager);
+  void* (*element_allocator_)(size_t count, void* manager);
+  void (*deallocator_)(void* ptr, void* manager);
+};
--- src/allocator.h
+++ src/allocator.h
@@ -1,4 +1,7 @@
 #ifndef KMC_C89_COMPILER_ALLOCATOR_H
 #define KMC_C89_COMPILER_ALLOCATOR_H
 
+/* reference to allocator */
+typedef const struct Allocator* AllocatorRef;
+
 #endif  /* KMC_C89_COMPILER_ALLOCATOR_H */
