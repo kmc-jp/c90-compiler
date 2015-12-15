a6c4373bb0dfe7f2ffd7fe498e91aa688438d5af
hatsusato <knightspm5@gmail.com>
2015-12-16 03:44:13 +0900

    Use ast_is_* to determine appropriate argument

--- src/ast/lexical_elements.c
+++ src/ast/lexical_elements.c
@@ -1,6 +1,7 @@
 #include "make_method.h"
 #include <assert.h>
 #include "ast_impl.h"
+#include "is_method.h"
 #include "pool.h"
 
 struct AstIdentifier {
@@ -34,7 +35,7 @@ struct AstStringLiteral {
 AstRef ast_make_identifier(AstRef token) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_token(token)) {
     AstIdentifierRef data = ast_palloc(struct AstIdentifier);
     data->token = token;
     self = ast_palloc(struct Ast);
@@ -47,7 +48,10 @@ AstRef ast_make_identifier(AstRef token) {
 AstRef ast_make_constant(AstRef constant) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_floating_constant(constant) ||
+      ast_is_integer_constant(constant) ||
+      ast_is_enumeration_constant(constant) ||
+      ast_is_character_constant(constant)) {
     AstConstantRef data = ast_palloc(struct AstConstant);
     data->constant = constant;
     self = ast_palloc(struct Ast);
@@ -60,7 +64,7 @@ AstRef ast_make_constant(AstRef constant) {
 AstRef ast_make_floating_constant(AstRef token) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_token(token)) {
     AstFloatingConstantRef data = ast_palloc(struct AstFloatingConstant);
     data->token = token;
     self = ast_palloc(struct Ast);
@@ -73,7 +77,7 @@ AstRef ast_make_floating_constant(AstRef token) {
 AstRef ast_make_integer_constant(AstRef token) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_token(token)) {
     AstIntegerConstantRef data = ast_palloc(struct AstIntegerConstant);
     data->token = token;
     self = ast_palloc(struct Ast);
@@ -86,7 +90,7 @@ AstRef ast_make_integer_constant(AstRef token) {
 AstRef ast_make_enumeration_constant(AstRef identifier) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_identifier(identifier)) {
     AstEnumerationConstantRef data = ast_palloc(struct AstEnumerationConstant);
     data->identifier = identifier;
     self = ast_palloc(struct Ast);
@@ -99,7 +103,7 @@ AstRef ast_make_enumeration_constant(AstRef identifier) {
 AstRef ast_make_character_constant(AstRef token) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_token(token)) {
     AstCharacterConstantRef data = ast_palloc(struct AstCharacterConstant);
     data->token = token;
     self = ast_palloc(struct Ast);
@@ -112,7 +116,7 @@ AstRef ast_make_character_constant(AstRef token) {
 AstRef ast_make_string_literal(AstRef token) {
   AstRef self = NULL;
   assert(token);
-  {
+  if (ast_is_token(token)) {
     AstStringLiteralRef data = ast_palloc(struct AstStringLiteral);
     data->token = token;
     self = ast_palloc(struct Ast);
