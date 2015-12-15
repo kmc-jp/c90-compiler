54b955273e4afe175b42d01cfa966a7b0aff5ab0
hatsusato <knightspm5@gmail.com>
2015-12-15 15:31:36 +0900

    Add ast_pool_{ctor,dtor}

--- src/ast/pool.c
+++ src/ast/pool.c
@@ -2,6 +2,15 @@
 
 static MemoryPoolRef g_ast_pool = NULL;
 
+void ast_pool_ctor(size_t size) {
+  assert(!g_ast_pool);
+  g_ast_pool = memory_pool_ctor(size);
+}
+
+void ast_pool_dtor(void) {
+  memory_pool_dtor(&g_ast_pool);
+}
+
 MemoryPoolRef ast_pool(void) {
   return g_ast_pool;
 }
--- src/ast/pool.h
+++ src/ast/pool.h
@@ -3,6 +3,8 @@
 
 #include "../memory_pool.h"
 
+void ast_pool_ctor(size_t size);
+void ast_pool_dtor(void);
 MemoryPoolRef ast_pool(void);
 
 #endif  /* KMC_C89_COMPILER_AST_POOL_H */
