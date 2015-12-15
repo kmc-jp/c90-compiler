0cb640c23b859f7e306c4ce892025e5282f66c1a
hatsusato <knightspm5@gmail.com>
2015-12-15 20:03:22 +0900

    Add struct definitions under Lexical elements section

---
+++ src/ast/lexical_elements.c
@@ -0,0 +1,22 @@
+#include "ast_impl.h"
+
+struct AstIdentifier {
+};
+
+struct AstConstant {
+};
+
+struct AstFloatingConstant {
+};
+
+struct AstIntegerConstant {
+};
+
+struct AstEnumerationConstant {
+};
+
+struct AstCharacterConstant {
+};
+
+struct AstStringLiteral {
+};
