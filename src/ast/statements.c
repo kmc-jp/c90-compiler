1e3e17934870fd4f2dd9d04c9b8934b137308250
hatsusato <knightspm5@gmail.com>
2015-12-15 20:05:32 +0900

    Add struct definitions under statements section

---
+++ src/ast/statements.c
@@ -0,0 +1,28 @@
+#include "ast_impl.h"
+
+struct AstStatement {
+};
+
+struct AstLabeledStatement {
+};
+
+struct AstCompoundStatement {
+};
+
+struct AstDeclarationList {
+};
+
+struct AstStatementList {
+};
+
+struct AstExpressionStatement {
+};
+
+struct AstSelectionStatement {
+};
+
+struct AstIterationStatement {
+};
+
+struct AstJumpStatement {
+};
