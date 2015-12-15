e99f27e82c783ac71249cc7efa5f5011c7a5f21f
hatsusato <knightspm5@gmail.com>
2015-12-16 03:13:11 +0900

    Fix wrong directory hierarchy

--- src/ast/make_method.h
+++ src/ast/make_method.h
@@ -1,7 +1,7 @@
 #ifndef KMC_C89_COMPILER_AST_MAKE_METHOD_H
 #define KMC_C89_COMPILER_AST_MAKE_METHOD_H
 
-#include "ast.h"
+#include "../ast.h"
 
 AstRef ast_make_identifier();
 AstRef ast_make_constant();
