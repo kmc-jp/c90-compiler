51d5ecb057604df24ad52e28c3b3f75e74306189
hatsusato <knightspm5@gmail.com>
2015-12-15 19:38:15 +0900

    Move definition of struct Ast

--- src/ast.c
+++ src/ast.c
@@ -1,8 +1,1 @@
 #include "ast.h"
-
-struct Ast {
-  enum AstTag tag;
-  union AstData {
-    char* token;
-  } data;
-};
--- src/ast/ast_impl.h
+++ src/ast/ast_impl.h
@@ -1,4 +1,11 @@
 #ifndef KMC_C89_COMPILER_AST_AST_IMPL_H
 #define KMC_C89_COMPILER_AST_AST_IMPL_H
 
+struct Ast {
+  enum AstTag tag;
+  union AstData {
+    char* token;
+  } data;
+};
+
 #endif  /* KMC_C89_COMPILER_AST_AST_IMPL_H */
