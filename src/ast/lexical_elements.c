e2e777f3666ab3e60d15c0bbd610976362b754d6
hatsusato <knightspm5@gmail.com>
2015-12-15 20:37:57 +0900

    Implement struct under lexical elements

--- src/ast/lexical_elements.c
+++ src/ast/lexical_elements.c
@@ -1,22 +1,29 @@
 #include "ast_impl.h"
 
 struct AstIdentifier {
+  AstRef token;
 };
 
 struct AstConstant {
+  AstRef constant;
 };
 
 struct AstFloatingConstant {
+  AstRef token;
 };
 
 struct AstIntegerConstant {
+  AstRef token;
 };
 
 struct AstEnumerationConstant {
+  AstRef identifier;
 };
 
 struct AstCharacterConstant {
+  AstRef token;
 };
 
 struct AstStringLiteral {
+  AstRef token;
 };
