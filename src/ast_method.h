e59283af9133fbe20dda67f3b9ffc79fec0053c2
hatsusato <knightspm5@gmail.com>
2015-12-16 22:12:15 +0900

    Include appropriate headers to ast_method.h

--- src/ast_method.h
+++ src/ast_method.h
@@ -1,8 +1,13 @@
 #ifndef KMC_C89_COMPILER_AST_METHOD_H
 #define KMC_C89_COMPILER_AST_METHOD_H
 
-#include "ast/get_method.h"
 #include "ast/is_method.h"
-#include "ast/make_method.h"
+#include "ast/get_method.h"
+#include "ast/lexical_elements.h"
+#include "ast/expressions.h"
+#include "ast/declarations.h"
+#include "ast/statements.h"
+#include "ast/external_definitions.h"
+#include "ast/ast_string.h"
 
 #endif  /* KMC_C89_COMPILER_AST_METHOD_H */
