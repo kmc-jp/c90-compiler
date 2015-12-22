d42d5b5ad83055f1c24edd6173c88e85c8f92065
hatsusato <knightspm5@gmail.com>
2015-12-22 23:52:51 +0900

    Fix ast make method prototypes

--- src/ast/lexical_elements.h
+++ src/ast/lexical_elements.h
@@ -3,12 +3,12 @@
 
 #include "../ast.h"
 
-AstRef ast_make_identifier();
-AstRef ast_make_constant();
-AstRef ast_make_floating_constant();
-AstRef ast_make_integer_constant();
-AstRef ast_make_enumeration_constant();
-AstRef ast_make_character_constant();
-AstRef ast_make_string_literal();
+AstRef ast_make_identifier(AstRef token);
+AstRef ast_make_constant(AstRef constant);
+AstRef ast_make_floating_constant(AstRef token);
+AstRef ast_make_integer_constant(AstRef token);
+AstRef ast_make_enumeration_constant(AstRef identifier);
+AstRef ast_make_character_constant(AstRef token);
+AstRef ast_make_string_literal(AstRef token);
 
 #endif  /* KMC_C89_COMPILER_AST_LEXICAL_ELEMENTS_H */
