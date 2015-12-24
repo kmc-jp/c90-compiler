938d03493ad6dcbe66de79e10587b271b05c9abe
prime <prime@kmc.gr.jp>
2015-12-25 06:12:57 +0900

    implement ast_make_external_declaration

--- src/ast/external_definitions.c
+++ src/ast/external_definitions.c
@@ -9,6 +9,7 @@ struct AstTranslationUnit {
 };
 
 struct AstExternalDeclaration {
+  AstRef external_declaration;
 };
 
 struct AstFunctionDefinition {
@@ -31,6 +32,19 @@ AstRef ast_push_translation_unit(
     AstTranslationUnitRef data = ast_get_translation_unit(translation_unit);
     ast_push_vector(data->external_declaration_vector, external_declaration);
     self = translation_unit;
+  }
+  return self;
+}
+
+AstRef ast_make_external_declaration(AstRef external_declaration) {
+  AstRef self = NULL;
+  if (ast_is_function_definition(external_declaration) ||
+      ast_is_declaration(external_declaration)) {
+    AstExternalDeclarationRef data = ast_palloc(struct AstExternalDeclaration);
+    data->external_declaration = external_declaration;
+    self = ast_palloc(struct Ast);
+    self->tag = AST_EXTERNAL_DECLARATION;
+    self->data.external_declaration = data;
   }
   return self;
 }
--- src/ast/external_definitions.h
+++ src/ast/external_definitions.h
@@ -5,7 +5,7 @@
 
 AstRef ast_make_translation_unit();
 AstRef ast_push_translation_unit(AstRef translation_unit, AstRef external_declaration);
-AstRef ast_make_external_declaration();
+AstRef ast_make_external_declaration(AstRef external_declaration);
 AstRef ast_make_function_definition();
 
 #endif  /* KMC_C89_COMPILER_AST_EXTERNAL_DEFINITIONS_H */
