c2a880593a0cd0ff61048588fa3251d7a684f5bf
prime <prime@kmc.gr.jp>
2015-12-25 06:08:27 +0900

    implement ast_make_translation_unit

--- src/ast/external_definitions.c
+++ src/ast/external_definitions.c
@@ -13,3 +13,12 @@ struct AstExternalDeclaration {
 
 struct AstFunctionDefinition {
 };
+
+AstRef ast_make_translation_unit() {
+  AstRef self = ast_palloc(struct Ast);
+  AstTranslationUnitRef data = ast_palloc(struct AstTranslationUnit);
+  data->external_declaration_vector = ast_make_vector();
+  self->tag = AST_TRANSLATION_UNIT;
+  self->data.translation_unit = data;
+  return self;
+}
