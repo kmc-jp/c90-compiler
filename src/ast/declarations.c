4e1eb191473fa6337a45113a633316b97bfba190
prime <prime@kmc.gr.jp>
2015-12-25 18:47:13 +0900

    implement ast_{make,push}_initializer_list

--- src/ast/declarations.c
+++ src/ast/declarations.c
@@ -125,15 +125,17 @@ AstRef ast_make_initializer(AstRef initializer) {
   return self;
 }
 
-AstRef ast_make_initializer_list(AstRef initializer_list, AstRef initializer) {
+AstRef ast_make_initializer_list(void) {
+  AstRef self = ast_palloc(struct Ast);
+  AstInitializerListRef data = ast_palloc(struct AstInitializerList);
+  data->initializer_vector = ast_make_vector();
+  self->tag = AST_INITIALIZER_LIST;
+  self->data.initializer_list = data;
+  return self;
+}
+
+AstRef ast_push_initializer_list(AstRef initializer_list, AstRef initializer) {
   AstRef self = NULL;
-  if (initializer_list == NULL) {
-    AstInitializerListRef data = ast_palloc(struct AstInitializerList);
-    data->initializer_vector = ast_make_vector();
-    initializer_list = ast_palloc(struct Ast);
-    initializer_list->tag = AST_INITIALIZER_LIST;
-    initializer_list->data.initializer_list = data;
-  }
   if (ast_is_initializer_list(initializer_list) &&
       ast_is_initializer(initializer)) {
     AstInitializerListRef data = ast_get_initializer_list(initializer_list);
