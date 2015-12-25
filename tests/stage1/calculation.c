537156d8762050fe0d499b464d0ad3fe3b73bdd8
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-12-26 04:50:00 +0900

    add calculation.c

---
+++ tests/stage1/calculation.c
@@ -0,0 +1,16 @@
+void print_num(int);
+void print(const char *);
+
+int main() {
+  int a;
+  a = 1 + 5 * 8 + 1;
+  print_num(a);
+  print("\n");
+  a = 100 / 200 * 200;
+  print_num(a);
+  print("\n");
+  a = 1 - 20 / 10 * 4 + 10;
+  print_num(a);
+  print("\n");
+  return 0;
+}
