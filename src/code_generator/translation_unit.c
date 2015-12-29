ae0d6d4ad5767985e7748c81382568464bc3b469
prime <prime@kmc.gr.jp>
2015-12-29 20:13:56 +0900

    add code_generator/translation_unit.c

---
+++ src/code_generator/translation_unit.c
@@ -0,0 +1,52 @@
+#include "code_generator.h"
+#include "ast/external_definitions_impl.h"
+#include "stdstring.h"
+#include "ast/is_method.h"
+#include "ast/get_method.h"
+#include "ast/ast_impl.h"
+
+LLVMTypeRef get_function_type(AstFunctionDefinitionRef function_definition);
+StringRef get_function_name(AstFunctionDefinitionRef function_definition);
+
+LLVMTypeRef get_type(AstRef ast) {
+  switch (ast->tag) {
+    case AST_DECLARATION_SPECIFIER_LIST:
+      return LLVMInt32Type();
+    default:
+      return NULL;
+  }
+}
+
+LLVMTypeRef get_function_type(AstFunctionDefinitionRef function_definition) {
+  LLVMTypeRef return_type = get_type(function_definition->declaration_specifier_list);
+  ParameterTypeRef parameter_types = get_parameter_types(function_definition->declarator);
+  return LLVMFunctionType(
+      return_type,
+      ParameterTypeFunc(data)(parameter_types),
+      ParameterTypeFunc(size)(parameter_types),
+      false);
+}
+
+void define_function(LLVMModuleRef module, LLVMBuilderRef builder,
+    AstFunctionDefinitionRef function_definition) {
+  StringRef name = get_function_name(function_definition);
+  LLVMTypeRef type = get_function_type(function_definition);
+  LLVMValueRef func = LLVMAddFunction(module, string_data(name), type);
+  LLVMBasicBlockRef entry = LLVMAppendBasicBlock(func, "entry");
+  LLVMPositionBuilderAtEnd(builder, entry);
+  build_block(module, builder, function_definition->compound_statement);
+}
+
+void translation_unit(LLVMModuleRef module, AstTranslationUnitRef translation_unit) {
+  AstVectorRef vector = translation_unit->external_declaration_vector;
+  LLVMBuilderRef builder = LLVMCreateBuilder();
+  int i;
+  for (i = 0; i < (int)AST_VECTOR_FUNC(size)(vector); ++i) {
+    AstRef external_declaration = AST_VECTOR_FUNC(at)(vector, i);
+    if (ast_is_function_definition(external_declaration)) {
+      define_function(module, builder, ast_get_function_definition(external_declaration));
+    } else {
+      /* declaration(ast_get_declaration(external_declaration)); */
+    }
+  }
+}
