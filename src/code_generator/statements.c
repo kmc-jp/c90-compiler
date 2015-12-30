924f050fa7b1b7800b1f7ac54034d2fb8a53ba01
prime <prime@kmc.gr.jp>
2015-12-30 09:09:05 +0900

    implement build_block_declaration

--- src/code_generator/statements.c
+++ src/code_generator/statements.c
@@ -1,6 +1,7 @@
 #include "code_generator.h"
 #include "variable_set.h"
 #include "ast/statements_impl.h"
+#include "ast/declarations_impl.h"
 #include "ast/is_method.h"
 #include "ast/get_method.h"
 #include "ast/ast_impl.h"
@@ -20,6 +21,13 @@ void build_block_declaration_list(
   for (i = 0; i < AST_VECTOR_FUNC(size)(declaration_vector); i++) {
     build_block(module, builder, variable_set, AST_VECTOR_FUNC(at)(declaration_vector, i));
   }
+}
+
+void build_block_declaration(
+    LLVMModuleRef module, LLVMBuilderRef builder, VariableSetRef variable_set,
+    AstDeclarationRef declaration) {
+  LLVMTypeRef type = get_type(declaration->declaration_specifier_list);
+  build_block_type(module, builder, variable_set, type, declaration->init_declarator_list);
 }
 
 void build_block_statement_list(
@@ -51,6 +59,8 @@ void build_block(LLVMModuleRef module, LLVMBuilderRef builder, VariableSetRef va
       build_block_compound_statement(module, builder, variable_set, ast_get_compound_statement(ast)); break;
     case AST_DECLARATION_LIST:
       build_block_declaration_list(module, builder, variable_set, ast_get_declaration_list(ast)); break;
+    case AST_DECLARATION:
+      build_block_declaration(module, builder, variable_set, ast_get_declaration(ast)); break;
     case AST_STATEMENT_LIST:
       build_block_statement_list(module, builder, variable_set, ast_get_statement_list(ast)); break;
     case AST_STATEMENT:
