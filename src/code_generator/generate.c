1ccc6b16ddc66431069a36f03755c243c14f39fd
hatsusato <knightspm5@gmail.com>
2016-06-12 22:40:22 +0900

    Implement generate_code

--- src/code_generator/generate.c
+++ src/code_generator/generate.c
@@ -1,1 +1,12 @@
 #include "generate.h"
+#include <assert.h>
+#include "ast/ast_impl.h"
+
+void generate_code(AstRef ast) {
+  assert(ast);
+  switch (ast->tag) {
+    default:
+      fprintf(stderr, "Unimplemented: %d\n", ast->tag);
+      break;
+  }
+}
