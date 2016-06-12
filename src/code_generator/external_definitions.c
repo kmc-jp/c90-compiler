0c2c5c6c2e610811a607b547c5d3e4e544c609a3
hatsusato <knightspm5@gmail.com>
2016-06-12 23:45:16 +0900

    Add generate_translation_unit

--- src/code_generator/external_definitions.c
+++ src/code_generator/external_definitions.c
@@ -1,1 +1,24 @@
 #include "code_generator/external_definitions.h"
+#include <assert.h>
+#include "ast/external_definitions_impl.h"
+#include "ast/is_method.h"
+#include "ast/pool.h"
+#include "code_generator/generate.h"
+#include "error.h"
+
+void generate_translation_unit(AstTranslationUnitRef translation_unit) {
+  assert(translation_unit);
+  {
+    const AstVectorRef vector = translation_unit->external_declaration_vector;
+    const AstRef* const begin = AST_VECTOR_FUNC(begin)(vector);
+    const AstRef* const end = AST_VECTOR_FUNC(end)(vector);
+    const AstRef* iter = NULL;
+    for (iter = begin; iter != end; ++iter) {
+      if (ast_is_external_declaration(*iter)) {
+        generate_code(*iter);
+      } else {
+        error("Not translation_unit");
+      }
+    }
+  }
+}
--- src/code_generator/external_definitions.h
+++ src/code_generator/external_definitions.h
@@ -1,4 +1,8 @@
 #ifndef KMC_C90_COMPILER_CODE_GENERATOR_EXTERNAL_DEFINITIONS_H
 #define KMC_C90_COMPILER_CODE_GENERATOR_EXTERNAL_DEFINITIONS_H
 
+#include "ast.h"
+
+void generate_translation_unit(AstTranslationUnitRef translation_unit);
+
 #endif  /* KMC_C90_COMPILER_CODE_GENERATOR_EXTERNAL_DEFINITIONS_H */
