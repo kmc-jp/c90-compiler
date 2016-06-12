5e536541bf06573c7663e14798e4c12adf00b57e
hatsusato <knightspm5@gmail.com>
2016-06-13 01:43:22 +0900

    Add error

--- src/error.c
+++ src/error.c
@@ -1,1 +1,13 @@
 #include "error.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void error(const char* message, ...) {
+  va_list args;
+  va_start(args, message);
+  vfprintf(stderr, message, args);
+  va_end(args);
+  fprintf(stderr, "\n");
+  exit(EXIT_FAILURE);
+}
--- src/error.h
+++ src/error.h
@@ -1,4 +1,6 @@
 #ifndef KMC_C90_COMPILER_ERROR_H
 #define KMC_C90_COMPILER_ERROR_H
 
+void error(const char* message, ...);
+
 #endif  /* KMC_C90_COMPILER_ERROR_H */
