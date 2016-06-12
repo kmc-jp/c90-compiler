759c7b68a5505c6f9f4e183d7bd464e5c7210ab1
hatsusato <knightspm5@gmail.com>
2016-06-12 21:21:26 +0900

    Implement module_{ctor,dtor}

--- src/code_generator/module.c
+++ src/code_generator/module.c
@@ -1,1 +1,15 @@
 #include "module.h"
+#include <assert.h>
+
+static LLVMModuleRef g_module = NULL;
+
+void module_ctor(const char* module_name) {
+  assert(!g_module);
+  g_module = LLVMModuleCreateWithName(module_name);
+}
+
+void module_dtor(void) {
+  assert(g_module);
+  LLVMDisposeModule(g_module);
+  g_module = NULL;
+}
--- src/code_generator/module.h
+++ src/code_generator/module.h
@@ -1,4 +1,7 @@
 #ifndef KMC_C90_COMPILER_CODE_GENERATOR_MODULE_H
 #define KMC_C90_COMPILER_CODE_GENERATOR_MODULE_H
 
+void module_ctor(const char* module_name);
+void module_dtor(void);
+
 #endif  /* KMC_C90_COMPILER_CODE_GENERATOR_MODULE_H */
