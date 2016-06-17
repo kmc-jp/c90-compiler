a15066cf23d9270925f197d18e82673e642387cb
hatsusato <knightspm5@gmail.com>
2016-06-17 10:52:38 +0900

    Add struct SymbolTable

--- src/code_generator/symbol_table.h
+++ src/code_generator/symbol_table.h
@@ -3,5 +3,6 @@
 
 typedef struct SymbolInfo* SymbolInfoRef;
 typedef struct SymbolBlock* SymbolBlockRef;
+typedef struct SymbolTable* SymbolTableRef;
 
 #endif  /* KMC_C90_COMPILER_CODE_GENERATOR_SYMBOL_TABLE_H */
