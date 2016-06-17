abfd7bbbfd3ef4665e2d5a7a9fee32c371006b88
hatsusato <knightspm5@gmail.com>
2016-06-17 11:22:47 +0900

    Add symbol_info_{ctor,dtor}

--- src/code_generator/symbol_table.c
+++ src/code_generator/symbol_table.c
@@ -1,4 +1,23 @@
 #include "code_generator/symbol_table.h"
+#include "code_generator/symbol_table_impl.h"
+#include "stdstring.h"
+#include "utility.h"
 
 DEFINE_VECTOR(SymbolInfoRef)
 DEFINE_VECTOR(SymbolBlockRef)
+
+SymbolInfoRef symbol_info_ctor(StringRef name, LLVMTypeRef type,
+                               LLVMValueRef value) {
+  const SymbolInfoRef symbol = safe_malloc(struct SymbolInfo);
+  const StringRef symbol_name = string_ctor(string_data(name), NULL);
+  symbol->name = symbol_name;
+  symbol->type = type;
+  symbol->value = value;
+  return symbol;
+}
+
+void symbol_info_dtor(SymbolInfoRef* pself) {
+  assert(pself);
+  string_dtor(&(*pself)->name);
+  safe_free(*pself);
+}
