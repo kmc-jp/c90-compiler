85459cbe66b7eb29f26b5ddd97fcde9df318b92e
NoNameA 774 <nonamea.774@gmail.com>
2016-06-18 02:05:55 +0900

    define DOUBLE_to_string

--- utils/stringify.c
+++ utils/stringify.c
@@ -7,6 +7,16 @@
     printf("}\n");                           \
   }
 
+#define DOUBLE_to_string(NAME, TYPE, MEMBER1, MEMBER2) \
+  void NAME ## _to_string(TYPE val) {                  \
+    printf("TYPE : {\n");                              \
+    printf("MEMBER1 :");                               \
+    vidualizer(val->MEMBER1);                          \
+    printf("MEMBER2 :");                               \
+    vidualizer(val->MEMBER2);                          \
+    printf("}\n");                                     \
+  }
+
 SINGLE_to_string(identifier, AstIdentifierRef, token)
 SINGLE_to_string(constant, AstConstantRef, constant)
 SINGLE_to_string(floating_constant, AstFloatingConstantRef, token)
@@ -17,44 +27,10 @@ SINGLE_to_string(character_constant, AstCharacterConstantRef, token)
 SINGLE_to_string(string_literal, AstStringLiteralRef, token)
 SINGLE_to_string(primary_expression, AstPrimaryExpressionRef, primary_expression)
 SINGLE_to_string(postfix_expression, AstPostfixExpressionRef, postfix)
-
-void array_subscript_expression_to_string(AstArraySubscriptExpressionRef val) {
-  printf("AstArraySubscriptExpressionRef: {\n");
-  printf("array:");
-  vidualizer(val->array);
-  printf("subscript:");
-  vidualizer(val->subscript);
-  printf("}\n");
-}
-
-void function_call_expression_to_string(AstFunctionCallExpressionRef val) {
-  printf("AstFunctionCallExpressionRef: {\n");
-  printf("function:");
-  vidualizer(val->function);
-  printf("argument_list:");
-  vidualizer(val->argument_list);
-  printf("}\n");
-}
-
-void member_access_expression_to_string(AstMemberAccessExpressionRef val) {
-  printf("AstMemberAccessExpressionRef: {\n");
-  printf("object:");
-  vidualizer(val->object);
-  printf("member:");
-  vidualizer(val->member);
-  printf("}\n");
-}
-
-void member_access_through_pointer_expression_to_string(AstMemberAccessThroughPointerExpressionRef val) {
-  printf("AstMemberAccessThroughPointerExpressionRef: {\n");
-  printf("object:");
-  vidualizer(val->object);
-  printf("member:");
-  vidualizer(val->member);
-  printf("}\n");
-}
-
-
+DOUBLE_to_string(array_subscript_expression, AstArraySubscriptExpressionRef, array, subscript)
+DOUBLE_to_string(function_call_expression, AstFunctionCallExpressionRef, function, argument_list)
+DOUBLE_to_string(member_access_expression, AstMemberAccessExpressionRef, object, member)
+DOUBLE_to_string(member_access_through_pointer, AstMemberAccessThroughPointerExpressionRef, object, member)
 SINGLE_to_string(postfix_increment_expression, AstPostfixIncrementExpressionRef, postfix)
 SINGLE_to_string(postfix_decrement_expression, AstPostfixDecrementExpressionRef, postfix)
 
@@ -79,58 +55,46 @@ SINGLE_to_string(sizeof_expression, AstSizeofExpressionRef, unary)
 SINGLE_to_string(sizeof_type_expression, AstSizeofTypeExpressionRef, type_name)
 SINGLE_to_string(cast_expression, AstCastExpressionRef, cast)
 
-type_cast_expression_to_string()
+DOUBLE_to_string(type_cast_expression, AstTypeCastExpressionRef, type_name, expression)
 
 SINGLE_to_string(multiplicative_expression, AstMultiplicativeExpressionRef, expression)
 
-product_expression_to_string()
-
-division_expression_to_string()
-
-modulo_expression_to_string()
+DOUBLE_to_string(product_expression, AstProductExpressionRef, multiplicative, cast)
+DOUBLE_to_string(division_expression, AstDivisionExpressionRef, multiplicative, cast)
+DOUBLE_to_string(modulo_expression, AstModuloExpressionRef, multiplicative, cast)
 
 SINGLE_to_string(additive_expression, AstAdditiveExpressionRef, expression)
 
-addition_expression_to_string()
-
-subtraction_expression_to_string()
-
+DOUBLE_to_string(addition_expression, AstAdditionExpressionRef, additive, multiplicative)
+DOUBLE_to_string(subtraction_expression, AstSubtractionExpressionRef, additive, multiplicative)
 SINGLE_to_string(shift_expression, AstShiftExpressionRef, expression)
 
-left_shift_expression_to_string()
-right_shift_expression_to_string()
+DOUBLE_to_string(left_shift_expression, AstLeftShiftExpressionRef, shift, additive)
+DOUBLE_to_string(right_shift_expression, AstRightShiftExpressionRef, shift, additive)
 
 SINGLE_to_string(relational_expression, AstRelationalExpressionRef, expression)
 
-less_than_expression_to_string()
-greater_than_expression_to_string()
-less_than_or_equal_to_expression_to_string()
-greater_than_or_equal_to_expression_to_string()
+DOUBLE_to_string(less_than_expression, AstLessThanExpressionRef, relational, shift)
+DOUBLE_to_string(greater_than_expression, AstGreaterThanExpressionRef, relational, shift)
+DOUBLE_to_string(less_than_or_equal_to_expression, AstLessThanOrEqualToExpressionRef, relational, shift)
+DOUBLE_to_string(greater_than_or_equal_to_expression, AstGreaterThanOrEqualToExpressionRef, relational, shift)
 
 SINGLE_to_string(equality_expression, AstEqualityExpressionRef, expression)
 
-equal_to_expression_to_string()
-not_equal_to_expression_to_string()
+DOUBLE_to_string(equal_to_expression, AstEqualToExpressionRef, equality, relational)
+DOUBLE_to_string(not_equal_to_expression, AstNotEqualToExpressionRef, equality, relational)
 
 SINGLE_to_string(bitwise_and_expression, AstBitwiseAndExpressionRef, expression)
-
-bitwise_and_operator_expression_to_string()
-
+DOUBLE_to_string(bitwise_and_operator_expression, AstBitwiseAndOperatorExpressionRef, bitwise_and, equality)
 SINGLE_to_string(bitwise_xor_expression, AstBitwiseXorExpressionRef, expression)
-
-bitwise_xor_operator_expression_to_string()
-
+DOUBLE_to_string(bitwise_xor_operator_expression, AstBitwiseXorOperatorExpressionRef, bitwise_xor, bitwise_and)
 SINGLE_to_string(bitwise_or_expression, AstBitwiseOrExpressionRef, expression)
-
-bitwise_or_operator_expression_to_string()
+DOUBLE_to_string(bitwise_or_operator_expression, AstBitwiseOrOperatorExpressionRef, bitwise_or, bitwise_xor)
 
 SINGLE_to_string(logical_and_expression, AstLogicalAndExpressionRef, expression)
-
-logical_and_operator_expression_to_string()
-
+DOUBLE_to_string(logical_and_operator_expression, AstLogicalAndOperatorExpressionRef, logical_and, bitwise_or)
 SINGLE_to_string(logical_or_expression, AstLogicalOrExpressionRef, expression)
-
-logical_or_operator_expression_to_string()
+DOUBLE_to_string(logical_or_operator_expression, AstLogicalOrOperatorExpressionRef, logical_or, logical_and)
 
 SINGLE_to_string(conditional_expression, AstConditionalExpressionRef, expression)
 
@@ -138,28 +102,25 @@ conditional_operator_expression_to_string()
 
 SINGLE_to_string(assignment_expression, AstAssignmentExpressionRef, expression)
 
-basic_assignment_expression_to_string()
+DOUBLE_to_string(basic_assignment_expression, AstBasicAssignmentExpressionRef, unary, assignment)
 
-ion_to_stringmultiplication_assignment_expression_to_string↵
-
-multiplication_assignment_expression_to_string()
-division_assignment_expression_to_string()
-modulo_assignment_expression_to_string()
-addition_assignment_expression_to_string()
-subtraction_assignment_expression_to_string()
-left_shift_assignment_expression_to_string()
-right_shift_assignment_expression_to_string()
-bitwise_and_assignment_expression_to_string()
-bitwise_xor_assignment_expression_to_string()
-bitwise_or_assignment_expression_to_string()
+DOUBLE_to_string(multiplication_assignment_expression, AstMultiplicationAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(division_assignment_expression, AstDivisionAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(modulo_assignment_expression, AstModuloAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(addition_assignment_expression, AstAdditionAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(subtraction_assignment_expression, AstSubtractionAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(left_shift_assignment_expression, AstLeftShiftAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(right_shift_assignment_expression, AstRightShiftAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(bitwise_and_assignment_expression, AstBitwiseAndAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(bitwise_xor_assignment_expression, AstBitwiseXorAssignmentExpressionRef, unary, assignment)
+DOUBLE_to_string(bitwise_or_assignment_expression, AstBitwiseOrAssignmentExpressionRef, unary, assignment)
 
 SINGLE_to_string(expression, AstExpressionRef, expression)
-
-comma_expression_to_string()
+DOUBLE_to_string(comma_expression, AstCommaExpressionRef, expression, assignment)
 
 SINGLE_to_string(constant, AstConstantExpressionRef, conditional)
 
-declaration_to_string()
+DOUBLE_to_string(declaration, AstDeclarationRef, declaration_specifier_list, init_declarator_list)
 declaration_specifier_list_to_string() // vector!!
 
 SINGLE_to_string(declaration_specifier, AstDeclarationSpecifierRef, declaration_specifier)
@@ -167,32 +128,32 @@ SINGLE_to_string(declaration_specifier, AstDeclarationSpecifierRef, declaration_
 init_declarator_list_to_string() // vec
 
 SINGLE_to_string(init_declarator, AstInitDeclaratorRef, init_declarator)
-declarator_with_initializer_to_string()
+DOUBLE_to_string(declarator_with_initializer, AstDeclaratorWithInitializerRef, declarator, initializer)
 
 SINGLE_to_string(storage_class_specifier, AstStorageClassSpecifierRef, storage_class_specifier)
 SINGLE_to_string(type_specifier, AstTypeSpecifierRef, type_specifier)
 SINGLE_to_string(struct_or_union_specifier, AstStructOrUnionSpecifierRef, struct_or_union_specifier)
 struct_or_union_definition_to_string()
-struct_or_union_declaration_to_string()
+DOUBLE_to_string(struct_or_union_declaration, AstStructOrUnionDeclarationRef, struct_or_union, identifier)
 SINGLE_to_string(struct_or_union, AstStructOrUnionRef, struct_or_union)
 struct_declaration_list_to_string() // vec
-struct_declaration_to_string()
+DOUBLE_to_string(struct_declaration, AstStructDeclarationRef, specifier_qualifier_list, struct_declarator_list)
 specifier_qualifier_list_to_string() // vev
 SINGLE_to_string(specifier_qualifier, AstSpecifierQualifierRef, specifier_qualifier)
 struct_declarator_list_to_string()
 SINGLE_to_string(struct_declarator, AstStructDeclaratorRef, struct_declarator)
 bit_field_declarator_to_string()
 SINGLE_to_string(enum_specifier, AstEnumSpecifierRef, enum_specifier)
-enum_definition_to_string()
+ppenum_definition_to_string()
 SINGLE_to_string(enum_declaration, AstEnumDeclarationRef, identifier)
 enumerator_list_to_string() // vec
 SINGLE_to_string(enumerator, AstEnumeratorRef, enumerator)
-enumerator_with_initializer_to_string()
+DOUBLE_to_string(enumerator_with_initializer, AstEnumeratorWithInitializerRef, enumeration_constant, constant_expression)
 SINGLE_to_string(type_qualifier, AstTypeQualifierRef, type_qualifier)
 declarator_to_string()
 SINGLE_to_string(direct_declarator, AstDirectDeclaratorRef, direct_declarator)
 array_declarator_to_string()
-function_declarator_to_string()
+DOUBLE_to_string(function_declarator, AstFunctionDeclaratorRef, direct_declarator, parameter_type_list)
 old_style_function_declarator_to_string()
 pointer_to_string()
 type_qualifier_list_to_string() //vec
@@ -200,7 +161,7 @@ SINGLE_to_string(parameter_type_list, AstParameterTypeListRef, parameter_type_li
 SINGLE_to_string(variadic_parameter_list, AstVariadicParameterListRef, parameter_list)
 parameter_list_to_string() // vec
 SINGLE_to_string(parameter_declaration, AstParameterDeclarationRef, parameter_declaration)
-parameter_concrete_declaration_to_string()
+DOUBLE_to_string(parameter_concrete_declaration, AstParameterConcreteDeclarationRef, declaration_specifier_list, declarator)
 parameter_abstract_declaration_to_string()
 identifier_list_to_string()
 type_name_to_string()
@@ -214,20 +175,20 @@ SINGLE_to_string(initializer, AstInitializerRef, initializer)
 initializer_list_to_string() // vec
 SINGLE_to_string(statement, AstStatementRef, statement)
 SINGLE_to_string(labeled_statement, AstLabeledStatementRef, labeled_statement)
-identifier_labeled_statement_to_string()
-case_labeled_statement_to_string()
+DOUBLE_to_string(identifier_labeled_statement, AstIdentifierLabeledStatementRef, identifier, statement)
+DOUBLE_to_string(case_labeled_statement, AstCaseLabeledStatementRef, constant_expression, statement)
 SINGLE_to_string(default_labeled_statement, AstDefaultLabeledStatementRef, statement)
-compound_statement_to_string()
+DOUBLE_to_string(compound_statement, AstCompoundStatementRef, declaration_list, statement_list)
 declaration_list_to_string() // vec
 statement_list_to_string() // vec
 expression_statement_to_string()
 SINGLE_to_string(selection_statement, AstSelectionStatementRef, selection_statement)
-if_statement_to_string()
+DOUBLE_to_string(if_statement, AstIfStatementRef, expression, statement)
 if_else_statement_to_string()
-switch_statement_to_string()
+DOUBLE_to_string(switch_statement, AstSwitchStatementRef, expression, statement)
 SINGLE_to_string(iteration_statement, AstIterationStatementRef, iteration_statement)
-while_statement_to_string()
-do_while_statement_to_string()
+DOUBLE_to_string(while_statement, AstWhileStatementRef, expression, statement)
+DOUBLE_to_string(do_while_statement, AstDoWhileStatementRef, expression, statement)
 for_statement_to_string()
 SINGLE_to_string(jump_statement, AstJumpStatementRef, jump_statement)
 SINGLE_to_string(goto_jump_statement, AstGotoJumpStatementRef, identifier)
