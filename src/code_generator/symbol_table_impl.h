7820fcb00ae47e68f3f46b78430462a2ac5dd0e7
hatsusato <knightspm5@gmail.com>
2016-06-17 11:20:05 +0900

    Fix symbol_table_impl.h
    Add missing headers

--- src/code_generator/symbol_table_impl.h
+++ src/code_generator/symbol_table_impl.h
@@ -1,20 +1,24 @@
 #ifndef KMC_C90_COMPILER_CODE_GENERATOR_SYMBOL_TABLE_IMPL_H
 #define KMC_C90_COMPILER_CODE_GENERATOR_SYMBOL_TABLE_IMPL_H
 
+#include <llvm-c/Core.h>
+#include "stdstring.h"
+#include "vector.h"
+
 struct SymbolInfo {
   StringRef name;
   LLVMTypeRef type;
   LLVMValueRef value;
 };
 
-DECLARE_VECTOR(SymbolInfoRef);
+DECLARE_VECTOR(SymbolInfoRef)
 
 struct SymbolBlock {
   StringRef name;
   VECTORREF(SymbolInfoRef) symbols;
 };
 
-DECLARE_VECTOR(SymbolBlockRef);
+DECLARE_VECTOR(SymbolBlockRef)
 
 struct SymbolTable {
   VECTORREF(SymbolBlockRef) stack;
