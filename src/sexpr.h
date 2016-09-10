6b4df125885320982e3df5b858a634364b0ed747
hatsusato <knightspm5@gmail.com>
2016-09-10 22:27:16 +0900

    Add struct ConsData

--- src/sexpr.h
+++ src/sexpr.h
@@ -1,4 +1,12 @@
 #ifndef KMC_C90_COMPILER_SEXPR_H
 #define KMC_C90_COMPILER_SEXPR_H
 
+struct Sexpr;
+typedef struct Sexpr* SexprRef;
+
+struct ConsData {
+  SexprRef car;
+  SexprRef cdr;
+};
+
 #endif  /* KMC_C90_COMPILER_SEXPR_H */
