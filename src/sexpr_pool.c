b6c2a20e42f70a9d5d4d955bd29a251808af6794
hatsusato <knightspm5@gmail.com>
2016-09-10 21:48:17 +0900

    Add sexpr_(initialize|finalize)_pool

--- src/sexpr_pool.c
+++ src/sexpr_pool.c
@@ -1,1 +1,19 @@
 #include "sexpr_pool.h"
+#include <assert.h>
+
+static const size_t SEXPR_POOL_CHUNK_SIZE = 1024;
+static MemoryPoolRef g_sexpr_pool = NULL;
+
+MemoryPoolRef sexpr_pool(void) {
+  assert(g_sexpr_pool);
+  return g_sexpr_pool;
+}
+
+void sexpr_initialize_pool(void) {
+  assert(!g_sexpr_pool);
+  g_sexpr_pool = memory_pool_ctor(SEXPR_POOL_CHUNK_SIZE);
+}
+
+void sexpr_finalize_pool(void) {
+  memory_pool_dtor(&g_sexpr_pool);
+}
--- src/sexpr_pool.h
+++ src/sexpr_pool.h
@@ -1,4 +1,10 @@
 #ifndef KMC_C90_COMPILER_SEXPR_POOL_H
 #define KMC_C90_COMPILER_SEXPR_POOL_H
 
+#include "memory_pool.h"
+
+MemoryPoolRef sexpr_pool(void);
+void sexpr_initialize_pool(void);
+void sexpr_finalize_pool(void);
+
 #endif  /* KMC_C90_COMPILER_SEXPR_POOL_H */
