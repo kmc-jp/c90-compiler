973d4f6668e8e3012cf6262824d0a0bffdbbfacd
hatsusato <knightspm5@gmail.com>
2016-09-16 10:19:15 +0900

    Add ast_info

--- src/ast.c
+++ src/ast.c
@@ -1,1 +1,10 @@
 #include "ast.h"
+#include <assert.h>
+
+const char* ast_info(enum AstTag tag) {
+  static const char* const tag_info[] = {
+    "IDENTIFIER",
+  };
+  assert(sizeof(tag_info)/sizeof(char*) == AST_TAG_ENUM_END);
+  return tag_info[tag];
+}
--- src/ast.h
+++ src/ast.h
@@ -2,7 +2,10 @@
 #define KMC_C90_COMPILER_AST_H
 
 enum AstTag {
-  AST_IDENTIFIER
+  AST_IDENTIFIER,
+  AST_TAG_ENUM_END
 };
+
+const char* ast_info(enum AstTag tag);
 
 #endif  /* KMC_C90_COMPILER_AST_H */
