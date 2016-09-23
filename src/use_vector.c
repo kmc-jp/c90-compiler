4bd736666c49f386b160ad67e5888ec3548c4fe3
hatsusato <knightspm5@gmail.com>
2016-09-23 13:31:33 +0900

    Use vector of string

--- src/use_vector.c
+++ src/use_vector.c
@@ -1,2 +1,4 @@
 #include "use_vector.h"
 #include "utility.h"
+
+DEFINE_VECTOR(StringRef)
--- src/use_vector.h
+++ src/use_vector.h
@@ -1,6 +1,9 @@
 #ifndef KMC_C90_COMPILER_USE_VECTOR_H
 #define KMC_C90_COMPILER_USE_VECTOR_H
 
+#include "stdstring.h"
 #include "vector.h"
+
+DECLARE_VECTOR(StringRef)
 
 #endif  /* KMC_C90_COMPILER_USE_VECTOR_H */
